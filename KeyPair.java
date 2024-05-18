b6f512f532089a51e9f2c556528ac8e1c9d2ca8f
Vijay Lakshminarayanan <laksvij@hawk.iit.edu>
2024-05-18 15:23:22 +0530

    fix upload

--- KeyPair.java
+++
@@ -1,9 +0,0 @@
-package com.lvijay.robotonous;
-
-public record KeyPair(char open, char close) {
-    public static KeyPair fromString(String v) {
-        if (v.length() != 2) { throw new IllegalArgumentException(); }
-
-        return new KeyPair(v.charAt(0), v.charAt(1));
-    }
-}
